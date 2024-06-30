66b5ffeb8de703375227bb6912a23fc27dad93f5
bakemoNie <em.verag@duocuc.cl>
2024-06-30 19:30:48 -0400

    creacion y migracion de tablas cliente y reserva a la bd

--- clientes/models.py
+++ clientes/models.py
@@ -1,11 +1,27 @@
 from django.db import models
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 
 class Cliente(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user   = models.OneToOneField(User, on_delete=models.CASCADE)
     nombre = models.CharField(max_length=100)
     apellido = models.CharField(max_length=100)
     fecha_nacimiento = models.DateField()
     email = models.EmailField()
     password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return str(self.nombre)+" "+str(self.apellido_paterno) 
+
+
+class Reserva(models.Model):
+    cli_reserva = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=100)
+    email = models.EmailField()
+    telefono = models.CharField(max_length=45)
+    fecha_reserva = models.DateField()
+    hora = models.TimeField()
+
+    def __str__(self):
+        return f'Reserva de {self.nombre} para el {self.fecha_reserva} a las {self.hora}'
