d11efd70ebe0de4bce8595c769bc41178bbb9990
bakemoNie <em.verag@duocuc.cl>
2024-07-04 15:35:08 -0400

    valis

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,14 +5,13 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('clientes.urls')),
+
     path('', views.index, name='index'),
     path('comidas/', views.comidas, name='comidas'),
     path('promociones/', views.promociones, name='promociones'),
-    path('reservacion/', views.reservacion, name='reservacion'),
     path('sucursal/', views.sucursal, name='sucursal'),
 
     path('accounts/', include("django.contrib.auth.urls")),
     
-
 
 ]
